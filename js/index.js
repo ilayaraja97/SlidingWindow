8d6264352ec63d6b90e746a9085b45dde58e1127
S Ilayaraja <2015kucp1030@iiitkota.ac.in>
2017-10-30 03:31:00 +0530

    wrap around update

--- js/index.js
+++ js/index.js
@@ -1,197 +1,230 @@
-function startSimulation(windowSize,frequency,delay,timeout) {
-    console.log("window size : "+windowSize)
-    console.log("frequency of packets : "+frequency);
-    console.log("delay : ",delay);
-    console.log("timeout : ",timeout);
-    windowSize=parseInt(windowSize);
-    frequency=parseFloat(frequency);
-    delay=parseFloat(delay);
-    timeout=parseFloat(timeout);
+function startSimulation(windowSize, frequency, delay, timeout) {
+    console.log("window size : " + windowSize)
+    console.log("frequency of packets : " + frequency);
+    console.log("delay : ", delay);
+    console.log("timeout : ", timeout);
+    document.querySelector("#start-stop-button").onclick = stop;
+    document.querySelector("#start-stop-button").value = "Stop";
+    windowSize = parseInt(windowSize);
+    frequency = parseFloat(frequency);
+    delay = parseFloat(delay);
+    timeout = parseFloat(timeout);
+
     paper.setup('simulation-frame');
     var senderFrames = [];
-    for(var i=0;i<30;i++){
+    for (var i = 0; i < 30; i++) {
         var o = {
-            path:new Path.Rectangle([5+25*i, 10], [20, 30]),
-            start:-1,
-            timer:timeout,
-            awk:false
+            path: new Path.Rectangle([5 + 25 * i, 10], [20, 30]),
+            start: -1,
+            timer: timeout,
+            awk: false,
+            text: new PointText({
+                point: new Point(5 + 25 * i + 10, 50),
+                justification: 'center',
+                fontSize: 10,
+                fillColor: 'black',
+                content: 'af'
+            }),
+            seqno: new PointText({
+                point: new Point(5 + 25 * i + 10, 25),
+                justification: 'center',
+                fontSize: 14,
+                fillColor: 'black',
+                content: i % (2 * windowSize)
+            })
         };
         o.path.strokeColor = 'black';
         senderFrames.push(o);
     }
-    var senderWindow = new Path.Rectangle([3, 8], [25*windowSize-1, 34]);
+    var senderWindow = new Path.Rectangle([3, 8], [25 * windowSize - 1, 34]);
     //console.log(senderWindow);
-    senderWindow.strokeColor='red'; 
+    senderWindow.strokeColor = 'red';
     var recieverFrames = [];
-    for(var i=0;i<30;i++){
+    for (var i = 0; i < 30; i++) {
         var o = {
-            path:new Path.Rectangle([5+25*i, 500-40], [20, 30]),
-            awk:false
+            path: new Path.Rectangle([5 + 25 * i, 500 - 40], [20, 30]),
+            awk: false,
+            seqno: new PointText({
+                point: new Point(5 + 25 * i + 10, 500 - 40 + 20),
+                justification: 'center',
+                fontSize: 14,
+                fillColor: 'black',
+                content: i % (2 * windowSize)
+            })
         };
         o.path.strokeColor = 'black';
         recieverFrames.push(o);
     }
-    var receiverWindow = new Path.Rectangle([3, 500-42], [25*windowSize-1, 34]);
-    receiverWindow.strokeColor='red'; 
+    var receiverWindow = new Path.Rectangle([3, 500 - 42], [25 * windowSize - 1, 34]);
+    receiverWindow.strokeColor = 'red';
 
-    var magic=-2,m1=1;
-    switch(windowSize)
-    {
-        case 2:
-        case 3:
-        break;
-        case 4:
-        case 5:magic=-1;
-        break;
-        case 6:
-        case 7:m1=1;magic=0;
-        break;
-        case 8:m1=2;magic=0;
-        break;
-        case 9:
-        case 10:m1=1;magic=1;
-        break;
-        default:magic=2;
-    }
-
-    var packet = [];    
-    view.onMouseDown = function(event) {
+    var translatex = 0;
+    var packet = [];
+    view.onMouseDown = function (event) {
         packet.forEach(function (e) {
             //console.log(e);
-            if(Math.abs(e.path.position.x-event.point.x)<20)
-                if(Math.abs(e.path.position.y-event.point.y)<30)
-                {   
-                    e.path.position.x=-15;
+            if (Math.abs(e.path.position.x - event.point.x) < 20)
+                if (Math.abs(e.path.position.y - event.point.y) < 30) {
+                    e.path.position.x = -15;
                     packet = packet.filter(item => item !== e);
                 }
-        },this);
-    } 
-    view.onFrame = function(event) {
-        var fps=60;
-        //get position of sender window
-        var spos=Math.round(senderWindow.position.x/25-3.6)-magic;
-        var rpos=Math.round(receiverWindow.position.x/25-3.6)-magic;
-        /*if(spos>=20)
-        {
-            spos=spos-15;
-            rpos=rpos-15;
-            senderWindow.position.x=senderWindow.position.x-375;
-            receiverWindow.position.x=receiverWindow.position.x-375;
-            packet.forEach(function (e){
-                e.path.position.x-=375;
-                e.id-=15;
-            },this);
-            for(var i=15;i<30;i++)
-            {
-                senderFrames[i-15].path.fillColor='#FFF';
-                recieverFrames[i-15].path.fillColor='#FFF';
-                senderFrames[i-15]=senderFrames[i];
-                recieverFrames[i-15]=recieverFrames[i];
-                var o = {
-                    path:new Path.Rectangle([5+25*i, 500-40], [20, 30]),
-                    awk:false
-                };
-                o.path.strokeColor = 'black';
-                recieverFrames[i]= o;
-                o = {
-                    path:new Path.Rectangle([5+25*i, 10], [20, 30]),
-                    start:-1,
-                    timer:timeout,
-                    awk:false
-                };
-                o.path.strokeColor = 'black';
-                senderFrames[i]=o;
-                
+        }, this);
+    }
+    var spos = 0, rpos = 0;
+    view.onFrame = function (event) {
+        var fps = 60;
+
+        //wrap around
+        if (rpos + windowSize + 1 > 30) {
+            // var canvas=document.querySelector("#simulation-frame").getContext('2d');
+            // canvas.;
+            for (var i = 0; i < 30; i++) {
+                senderFrames[i].path.position.x -= 450;
+                senderFrames[i].seqno.position.x -= 450;
+                senderFrames[i].text.position.x -= 450;
+                recieverFrames[i].path.position.x -= 450;
+                recieverFrames[i].seqno.position.x -= 450;
             }
-        }*/
-        if(event.time<2)
-        {
+            spos -= 18;
+            rpos -= 18;
+            senderWindow.position.x -= 450;
+            receiverWindow.position.x -= 450;
+            senderFrames = senderFrames.slice(18, 30);
+            recieverFrames = recieverFrames.slice(18, 30);
+            packet.forEach(function (e) {
+                e.path.position.x -= 450;
+                e.id -= 18;
+            });
+            var j = parseInt(senderFrames[0].seqno.content);
+            for (var i = 12; i < 30; i++) {
+                var o = {
+                    path: new Path.Rectangle([5 + 25 * i, 10], [20, 30]),
+                    start: -1,
+                    timer: timeout,
+                    awk: false,
+                    text: new PointText({
+                        point: new Point(5 + 25 * i + 10, 50),
+                        justification: 'center',
+                        fontSize: 10,
+                        fillColor: 'black',
+                        content: 'af'
+                    }),
+                    seqno: new PointText({
+                        point: new Point(5 + 25 * i + 10, 25),
+                        justification: 'center',
+                        fontSize: 14,
+                        fillColor: 'black',
+                        content: (j + i) % (2 * windowSize)
+                    })
+                };
+                o.path.strokeColor = 'black';
+                senderFrames.push(o);
+                o = {
+                    path: new Path.Rectangle([5 + 25 * i, 500 - 40], [20, 30]),
+                    awk: false,
+                    seqno: new PointText({
+                        point: new Point(5 + 25 * i + 10, 500 - 40 + 20),
+                        justification: 'center',
+                        fontSize: 14,
+                        fillColor: 'black',
+                        content: (j + i) % (2 * windowSize)
+                    })
+                };
+                o.path.strokeColor = 'black';
+                recieverFrames.push(o);
+            }
+        }/**/
+
+        if (event.time < 2) {
             //start simulation after 2s
         }
-        else if(parseInt(event.time*fps)%(frequency*fps)==0)
-        {
+        else if (parseInt(event.time * fps) % (frequency * fps) == 0) {
+
             //each send packets at a frequency
-            for(var i=0;i<=windowSize-m1;i++)
-            {
-                //console.log(spos+i);
-                if(spos+i>=30||spos+i<0)
+            for (var i = 0; i <= windowSize - 1; i++) {
+                if (spos + i >= 30 || spos + i < 0)
                     continue;
-                if(senderFrames[spos+i].start<0&&!senderFrames[spos+i].awk)
-                {
-                    senderFrames[spos+i].start=event.time;
-                    var o={
-                        path:senderFrames[spos+i].path.clone(),
-                        id:spos+i,
-                        awk:false
+                //console.log(spos+i);
+                if (senderFrames[spos + i].start < 0 && !senderFrames[spos + i].awk) {
+                    senderFrames[spos + i].start = event.time;
+                    var o = {
+                        path: senderFrames[spos + i].path.clone(),
+                        id: spos + i,
+                        awk: false
                     }
-                    o.path.fillColor='#333399';
+                    o.path.fillColor = '#333399';
                     packet.push(o);
                     break;
                 }
             }
-            for(var i=windowSize-1;i>0;i--)
-            {
-                //console.log(spos+i);
-            }
         }
-        
+
         //check if timeout has come for any sender frame
-        senderFrames.forEach(function(e) {
-            if(!e.awk&&e.start>0)
-            {
-                if(e.start+timeout<event.time)
-                {
-                    e.start=event.time;
-                    var o={
-                        path:e.path.clone(),
-                        id:senderFrames.indexOf(e),
-                        awk:false
+        senderFrames.forEach(function (e) {
+
+            if (!e.awk && e.start > 0) {
+                e.text.content = (timeout + e.start - event.time + "").substr(0, 4);
+                if (e.start + timeout < event.time) {
+                    e.start = event.time;
+                    var o = {
+                        path: e.path.clone(),
+                        id: senderFrames.indexOf(e),
+                        awk: false
                     }
                     //console.log(o);
-                    o.path.fillColor='#333399';
+                    o.path.fillColor = '#333399';
                     packet.push(o);
                 }
+            }
+            else {
+                e.text.content = '';
             }
         });
 
 
-        packet.forEach(function(e) {
-            if(true)
-            {
+        packet.forEach(function (e) {
+            if (e.id >= 0) {
                 //console.log("asd");
-                if(e.awk)
-                {
-                    if(e.path.position.y>10)
-                        e.path.position.y+=-450/(delay*fps);
-                    else
-                    {
-                        senderFrames[e.id].awk=true;
-                        e.path.position.y+=15;
+                if (e.awk) {
+                    if (e.path.position.y > 10)
+                        e.path.position.y += -450 / (delay * fps);
+                    else {
+                        senderFrames[e.id].awk = true;
+                        senderFrames[e.id].path.fillColor = '#66AAAA';
+                        e.path.position.x = -15;
                         packet = packet.filter(item => item !== e)
                     }
                 }
-                else
-                {
-                    if(e.path.position.y<460)
-                        e.path.position.y+=450/(delay*fps);
-                    else
-                    {
-                        recieverFrames[e.id].awk=true;
-                        recieverFrames[e.id].path.fillColor='#333399'
-                        e.awk=true;
-                        e.path.fillColor='#66AAAA'
+                else {
+                    if (e.path.position.y < 460)
+                        e.path.position.y += 450 / (delay * fps);
+                    else {
+                        recieverFrames[e.id].awk = true;
+                        recieverFrames[e.id].path.fillColor = '#333399'
+                        e.awk = true;
+                        e.path.fillColor = '#66AAAA'
                     }
                 }
             }
         }, this);
 
-        if(spos<30&&spos>=0)
-            if(senderFrames[spos].awk)
-                senderWindow.position.x+=25;
-        if(rpos<30&&rpos>=0)
-            if(recieverFrames[rpos].awk)
-                receiverWindow.position.x+=25;
-        
-        console.log(spos);
+        if (senderFrames[spos].awk) {
+            senderWindow.position.x += 25;
+            spos++;
+        }
+        if (recieverFrames[rpos].awk) {
+            receiverWindow.position.x += 25;
+            rpos++;
+        }
+
+        //console.log(spos);
     }
+}
+
+function stop() {
+    view.onFrame = function (event) { };
+    document.querySelector("#start-stop-button").value = "Refresh";
+    document.querySelector("#start-stop-button").onclick = function () {
+        window.location.reload();
+    };
 }
